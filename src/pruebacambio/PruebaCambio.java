e43c78e1b02d4b58c0c7f4cf18458bd1a0d2d84d
Wessly Zelaya <wess_zelaya@hotmail.com>
2022-07-31 15:12:48 -0600

    Agregando nuevas funciones de cajero

--- src/pruebacambio/PruebaCambio.java
+++ src/pruebacambio/PruebaCambio.java
@@ -6,8 +6,31 @@ public class PruebaCambio {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        Scanner opcion = new Scanner(System.in);
 
         int cantidad;
+        int seleccion;
+        int comodin=0;
+
+        do {
+            System.out.println("Porfavor seleccion una opcion");
+            System.out.println("    1. Consulta de saldo");
+            System.out.println("    2. Retiro de efectivo");
+            System.out.println("    3. Deposito de efectivo");
+            System.out.println("    4. Historial de transacciones");
+            System.out.println("    5. Salir del Sistema");
+            seleccion = opcion.nextInt();
+
+            if (seleccion >= 1 && seleccion <= 5) {
+                comodin = 1;
+            } else {
+                System.out.println("------------------------------------------");
+                System.out.println("Opcion No Disponible, Vuelva a intentar");
+                System.out.println("------------------------------------------");
+            }
+        } while (comodin == 0);
+
+
 
         System.out.print("Bienvenidos Ingrese la cantidad a desglosar: ");
         cantidad = entrada.nextInt();
@@ -17,7 +40,7 @@ public class PruebaCambio {
 
         for (int i = 0; i < billetes.length; i++) {
             if (cantidad >= billetes[i]) {
-                desglose[i] =(cantidad / billetes[i]);
+                desglose[i] = (cantidad / billetes[i]);
                 cantidad = cantidad - (desglose[i] * billetes[i]);
             }
         }
@@ -34,5 +57,5 @@ public class PruebaCambio {
         System.out.println("Muchas Gracias por usar nuestros servicios");
         System.out.println("-------------------------------------------");
     }
+
 }
-//El cajero automati
